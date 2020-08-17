089666d979395a8b30ab413421b141aae4dab39c
Oluwatobii <bellotobi10@gmail.com>
2020-08-17 22:24:35 +0000

    Solved Exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Type in the store you want to create"
+user_input = $stdin.gets.chomp.to_s
+store = Store.create(
+  name: "#{user_input}" 
+)
+puts store.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,19 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  validate :error_check
+ 
+  def apparel
+    mens_apparel || womens_apparel
+  end
+ 
+  def error_check
+    if !mens_apparel && !womens_apparel
+      errors.add(:apparel, "Cant be blank (Store policy requires Men/Women's Apparel)")
+    end
+  end
+  
 end

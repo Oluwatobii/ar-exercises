ee21c368b9caa48ceef9491e0f68140dc85d8261
Oluwatobii <bellotobi10@gmail.com>
2020-08-17 23:55:23 +0000

    Solved Exercise 8

--- exercises/exercise_8.rb
+++ exercises/exercise_8.rb
@@ -5,14 +5,11 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
+require_relative './exercise_7'
 
-puts "Exercise 7"
+puts "Exercise 8"
 puts "----------"
 
-# Your code goes here ...
-puts "Type in the store you want to create"
-user_input = $stdin.gets.chomp.to_s
-store = Store.create(
-  name: "#{user_input}"
-)
-puts store.errors.full_messages
+@store2.employees.create(first_name: "Lorelei", last_name: "Gaines", hourly_rate: 67)
+
+puts Employee.where(first_name: 'Markus').pluck("password") 
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,5 +1,15 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
+  
   validates :first_name, :last_name, :store_id, presence: true
   validates :hourly_rate, presence: true, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 } 
-  belongs_to :store
+
+  before_create :create_password
+
+  ## Enable an auto-generated password (string) when employees are added  in the database.
+  private
+    def create_password
+      self.password = (0...8).map { (65 + rand(26)).chr}.join
+    end
+
 end
